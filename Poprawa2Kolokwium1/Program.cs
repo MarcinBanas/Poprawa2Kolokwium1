2c5118c212678a934794e45749468fb5cb5aed91
Marcin Banaś <mb056625@student.ath.edu.pl>
2021-01-17 13:47:16 +0100

    Dodaj pliki projektów.

---
+++ Poprawa2Kolokwium1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Poprawa2Kolokwium1
+{
+    class Program
+    {
+        public void CzasPrzenoszenia(double dane)
+        {
+            if (dane > 16)
+            {
+                double iloscnosnikow = dane / 16;
+                double czaswgrywanie, czaszgrywanie;
+                czaswgrywanie = (dane * 1000) / 3;
+                czaszgrywanie = (dane * 1000) / 5;
+                Console.WriteLine("Czas:" + (czaswgrywanie + czaszgrywanie));
+
+            }
+            else
+            {
+                double czaswgrywanie, czaszgrywanie;
+                czaswgrywanie = (dane * 1000) / 3;
+                czaszgrywanie = (dane * 1000) / 5;
+                Console.WriteLine("Czas:" + (czaswgrywanie + czaszgrywanie));
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string[] tab = new string[5];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine("Podaj zyczenie:");
+                tab[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine("Numer:"+i+" Zyczenie: "+tab[i]);
+            }
+            string odp;
+            Console.WriteLine("Czy chcesz zmienic kolejnosc?(tak)");
+            odp = Console.ReadLine();
+            if (odp == "tak")
+            {
+                Console.WriteLine("podaj indeks elementu:");
+                int index;
+                index =int.Parse(Console.ReadLine());
+                Console.WriteLine("podaj nowy indeks elementu");
+                int index2;
+                index2 = int.Parse(Console.ReadLine());
+
+            }
+
+        }
+    }
+}
---
+++ Poprawa2Kolokwium1/SrodowiskoWdrozeniowe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poprawa2Kolokwium1
+{
+    class SrodowiskoWdrozeniowe
+    {
+        public string Nazwa { get; set; }
+        public string AdresIP { get; set; }
+        public int AktualnaWersja { get; set; }
+        public int PoprzedniaWersja { get; set; }
+
+        public SrodowiskoWdrozeniowe(string nazwa, string adresip, int aktualnawersja, int poprzedniawersja)
+        {
+            if (!nazwa.Equals("") && (Convert.ToString(adresip).Length == 15 && (Convert.ToString(adresip).Contains("..."))))
+            {
+                Nazwa = nazwa;
+                AdresIP = adresip;
+                AktualnaWersja = 1;
+                PoprzedniaWersja = 1;
+            }
+        }
+        public void Aktualizuj(int wersja)
+        {
+            if (wersja == AktualnaWersja)
+            {
+                Console.WriteLine("Podana wersja jest aktualna");
+            }
+            if (wersja > AktualnaWersja)
+            {
+                throw new ArgumentException("blad");
+            }
+            else
+            {
+                PoprzedniaWersja = AktualnaWersja;
+                AktualnaWersja = wersja;
+            }
+        }
+        public void WycofajZmiany()
+        {
+            Console.WriteLine("Wycofywanie zmian");
+            AktualnaWersja = PoprzedniaWersja;
+        }
+    }
+
+}
